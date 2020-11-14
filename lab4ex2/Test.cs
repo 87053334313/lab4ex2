b21c96c93693aa4911022060dc4dcb2c71a7aeaf
Victor Li <VictorLi@itcenterifmo.onmicrosoft.com>
2020-11-15 01:14:20 +0300

    Add project files.

---
+++ lab4ex2/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4ex2
+{
+    public class Utils 
+    {
+        public static void Swap(ref int a, ref int b) 
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+    public class Test
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Введи х");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введи y");
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine("Перед свапом x = "+x+" , y ="+y);
+        
+        Utils.Swap(ref x, ref y);
+
+        Console.WriteLine("После свапа(перестановки) x ="+x+" y = "+y);
+        Console.ReadKey();
+
+    }
+    }
+}
